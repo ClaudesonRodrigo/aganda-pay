da4034ab0ef7375c09b173bcafab59fab66b1481
Borges198 <seu_claudesonridrigo@gmail.com>
2026-06-21 11:31:20 -0300

    fix: corrigir ícones do manifesto

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,12 +13,12 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait', // Força a abertura sempre em pé (Mobile First)
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512.png',
+        src: '/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
